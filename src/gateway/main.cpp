1e51981a27e219fd5f9886c89bd3df45be9674a1
Graham <github.gpbenton@xoxy.net>
2024-04-29 11:27:19 +0100

    Add QuickEspNow startup code

--- src/gateway/main.cpp
+++ src/gateway/main.cpp
@@ -5,19 +5,32 @@
 #include <esp_wifi.h>
 #elif defined ESP8266
 #include <ESP8266WiFi.h>
-#define WIFI_MODE_STA WIFI_STA 
+#define WIFI_MODE_STA WIFI_STA
 #else
 #error "Unsupported platform"
 #endif //ESP32
+#include <QuickEspNow.h>
 
 #include "secrets.h"
 
 // put function declarations here:
-int myFunction(int, int);
+void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi, bool broadcast);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.printf("Connected to %s in channel %d\n", WiFi.SSID().c_str(), WiFi.channel());
+  Serial.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+  Serial.printf("MAC address: %s\n", WiFi.macAddress().c_str());
+  quickEspNow.onDataRcvd(dataReceived);
+  quickEspNow.begin(); // Use no parameters to start ESP-NOW on same channel as WiFi, in STA mode and synchronous send mode
 }
 
 void loop() {
@@ -25,6 +38,11 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi, bool broadcast) {
+    Serial.print ("Received: ");
+    Serial.printf ("%.*s\n", len, data);
+    Serial.printf ("RSSI: %d dBm\n", rssi);
+    Serial.printf ("From: " MACSTR "\n", MAC2STR(address));
+    Serial.printf ("%s\n", broadcast ? "Broadcast" : "Unicast");
 }
