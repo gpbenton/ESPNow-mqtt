d4321c34ca62e345cacf55f2732d222c262f3d34
Graham <github.gpbenton@xoxy.net>
2024-05-16 10:07:34 +0100

    Working battery_sensor code

--- src/battery_sensor/battery_sensor.cpp
+++ src/battery_sensor/battery_sensor.cpp
@@ -15,10 +15,13 @@
 
 // Send message every 2 seconds
 const unsigned int SEND_MSG_MSEC = 2000;
+const unsigned int WHOIS_RETRY_LIMIT = 5;
 RTC_DATA_ATTR int sharedChannel = 0 ;
+RTC_DATA_ATTR uint8_t gateway_address[6];
 RTC_DATA_ATTR int bootCount = 0;
 bool responseRcvd = false;
 bool msgSent = false;
+bool haveAddress = false;
 struct data msg;
 
 // put function declarations here:
@@ -35,14 +38,19 @@ void setup() {
   msg.wakeupCause = esp_sleep_get_wakeup_cause();
 
   switch(msg.wakeupCause){
-    case ESP_SLEEP_WAKEUP_EXT0 : break;
-    case ESP_SLEEP_WAKEUP_EXT1 : break; 
-    case ESP_SLEEP_WAKEUP_TIMER : 
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : break;
-    case ESP_SLEEP_WAKEUP_ULP : break;
+    case ESP_SLEEP_WAKEUP_EXT0:
+    case ESP_SLEEP_WAKEUP_EXT1:
+    case ESP_SLEEP_WAKEUP_TIMER: 
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+    case ESP_SLEEP_WAKEUP_ULP:
+      haveAddress = true;
       break;
     default:
+      // Power Up or reset so find correct channel
+      Serial.printf("Finding channel.  Wakeup cause %d\n", msg.wakeupCause);
       sharedChannel = getWiFiChannel(WIFI_SSID); 
+      Serial.printf("sharedChannel = %d\n", sharedChannel);
+      haveAddress = false;
       break;
   }
 
@@ -61,27 +69,35 @@ void setup() {
 
 void loop() {
 
-    static uint8_t retries = 0;
+  static uint8_t whoisretries = 0;
 
-    msg.batteryLevel = 50;
-    msg.sensor1 = sharedChannel;
-    msg.sensor2 = 0;
-    msg.sensor3 = 0;
-    comms_send_error_t err = COMMS_SEND_OK;   //quickEspNow.send(ESPNOW_BROADCAST_ADDRESS, (const unsigned char *)&msg, sizeof(msg));
-    if (!err)
-    {
-      sleep(5);
-      Serial.printf(">>>>>>>>>> Message sent: wakeCause = %d   bootCount= %d\n", msg.wakeupCause, bootCount);
-      delay(1);
-      gotoSleep(10);
+  msg.batteryLevel = whoisretries;
+  msg.sensor1 = sharedChannel;
+  msg.sensor2 = 0;
+  msg.sensor3 = 0;
+  if (haveAddress && !quickEspNow.send(gateway_address, (const unsigned char *)&msg, sizeof(msg))) {
+    Serial.printf(">>>>>>>>>> Message sent: wakeCause = %d   bootCount= %d\n", msg.wakeupCause, bootCount);
+    gotoSleep(10);
+  }
+  else {
+    Serial.printf(">>>>>>>>>> Message send failed\n");
+    haveAddress = false;
+
+    // look for the gateway on this channel
+    if (whoisretries < WHOIS_RETRY_LIMIT) {
+      Serial.printf("Sending Gateway query\n");
+      quickEspNow.send(ESPNOW_BROADCAST_ADDRESS, GATEWAY_QUERY, sizeof(GATEWAY_QUERY));
+      whoisretries++;
     }
-    else
-    {
-      Serial.printf(">>>>>>>>>> Message not sent: %d (0x%x)\n", err, err);
+    else {
+      // No gateway on this channel, look for the network id
+      Serial.printf("Searching for WiFi Channel\n");
+      sharedChannel = getWiFiChannel(WIFI_SSID);
+      whoisretries = 0;
     }
+  }
 
-    delay (SEND_MSG_MSEC);
-
+  delay(SEND_MSG_MSEC);
 }
 
 // put function definitions here:
@@ -104,13 +120,21 @@ void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi
   Serial.printf("RSSI: %d dBm\n", rssi);
   Serial.printf("From: " MACSTR "\n", MAC2STR(address));
   Serial.printf("%s\n", broadcast ? "Broadcast" : "Unicast");
+
+  if (!broadcast && !strncmp((const char *)data, (const char *)GATEWAY_QUERY, sizeof(GATEWAY_QUERY))) {
+    Serial.printf("Setting gateway address to " MACSTR "\n", MAC2STR(address));
+    for (int i=0; i<6; i++) {
+      gateway_address[i] = address[i];
+    }
+    Serial.printf("gateway_address = " MACSTR "\n", MAC2STR(gateway_address));
+    haveAddress = true;
+  }
 }
 
 void gotoSleep(long sleepTime){
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
 
   esp_sleep_enable_timer_wakeup(sleepTime * uS_TO_S_FACTOR);
-  delay(1000);
   Serial.flush();
   esp_deep_sleep_start();
 }
